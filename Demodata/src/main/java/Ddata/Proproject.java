a73c180a48c097449bed0abbc996d7d1ac9d357e
Palavelli Sreenu <palavellisreenu7@gmail.com>
2023-05-11 08:52:38 +0530

    Test part2

--- Demodata/src/main/java/Ddata/Proproject.java
+++ Demodata/src/main/java/Ddata/Proproject.java
@@ -5,5 +5,6 @@ public class Proproject
 public static void main(String[] args)
 {
 	System.out.println("Frame work created here");
+	System.out.println("Frame work updated");
 }
 }
---
+++ Demodata/src/main/java/Ddata/Proproject1.java
@@ -0,0 +1,9 @@
+package Ddata;
+
+public class Proproject1
+{
+public static void main(String[] args)
+{
+	System.out.println("Login done");
+}
+}
