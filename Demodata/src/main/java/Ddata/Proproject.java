b3fb5ed2d55fe2dbbfac4ce838df9a8f4959a901
Palavelli Sreenu <palavellisreenu7@gmail.com>
2023-05-11 09:15:58 +0530

    Test project3

--- Demodata/src/main/java/Ddata/Proproject.java
+++ Demodata/src/main/java/Ddata/Proproject.java
@@ -6,5 +6,6 @@ public static void main(String[] args)
 {
 	System.out.println("Frame work created here");
 	System.out.println("Frame work updated");
+	System.out.println("Frame review available");
 }
 }
