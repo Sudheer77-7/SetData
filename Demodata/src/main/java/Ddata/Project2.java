636d50d1b6697b6ee1736468a449d30dfba4d809
Palavelli Sreenu <palavellisreenu7@gmail.com>
2023-05-11 12:20:03 +0530

    Testdata10

--- Demodata/src/main/java/Ddata/Project2.java
+++ Demodata/src/main/java/Ddata/Project2.java
@@ -3,6 +3,6 @@ package Ddata;
 public class Project2
 {
 public static void main(String[] args) {
-	System.out.println("Profile module done in a sequence");
+	System.out.println("Profile module done in a Data");
 }
 }
---
+++ Demodata/src/main/java/Ddata/Project3.java
@@ -0,0 +1,9 @@
+package Ddata;
+
+public class Project3 
+{
+public static void main(String[] args)
+{
+	System.out.println("Dataupdated");
+}
+}
