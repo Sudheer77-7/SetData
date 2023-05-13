1c42fdce9aeb8fadd17ded0d41cc081de7c4a300
Palavelli Sreenu <palavellisreenu7@gmail.com>
2023-05-13 13:43:04 +0530

    Firstcommite jenkins

---
+++ jenkinsDemo/src/test/java/jenkinsDemo/Tc1.java
@@ -0,0 +1,13 @@
+package jenkinsDemo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Tc1
+{
+@Test
+public void Tcdata1()
+{
+	System.out.println("data enter");
+}
+}
---
+++ jenkinsDemo/src/test/java/jenkinsDemo/Tc2.java
@@ -0,0 +1,13 @@
+package jenkinsDemo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Tc2 {
+	
+		@Test
+		public void Tcdata2()
+		{
+			System.out.println("data enter already");
+		}
+}
